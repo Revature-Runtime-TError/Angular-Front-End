3b4b55dca8b7e314ce2884e19b0c76d367f1375c
Wesley Pruett <110426206+wespruett@users.noreply.github.com>
2022-10-17 13:29:52 -0600

    Fixed router error after moving component into component folder

--- social-media-angular/src/app/components/viewprofile/viewprofile.component.spec.ts
+++ social-media-angular/src/app/components/viewprofile/viewprofile.component.spec.ts
@@ -1,11 +1,7 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AuthService } from '../../services/auth.service';
-import { PostService } from '../../services/post.service';
-import { ProfileService } from '../../services/profile.service';
 
 import { ViewprofileComponent } from './viewprofile.component';
 
