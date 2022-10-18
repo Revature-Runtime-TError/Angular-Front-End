f125356dc81f7b71fcb7b68fccd82d7f8ccaf5ca
Wesley Pruett <110426206+wespruett@users.noreply.github.com>
2022-10-18 16:10:32 -0600

    change all urls to enviroment urls

--- social-media-angular/src/app/services/reset.service.ts
+++ social-media-angular/src/app/services/reset.service.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import User from '../models/User';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResetService {
 
-  private baseUrl: string = "http://localhost:8090/profile";
+  private profileUrl: string = `${environment.baseUrl}/profile`;
   constructor(private http: HttpClient) { }
 
   resetPassword(reset: User) {
-    return this.http.put<User>(this.baseUrl + "/reset", reset);
+    return this.http.put<User>(this.profileUrl + "/reset", reset);
   }
 
   findByEmail(email: string) {
-    return this.http.get<User>(this.baseUrl + "/fetch/" + email);
+    return this.http.get<User>(this.profileUrl + "/fetch/" + email);
   }
 }
